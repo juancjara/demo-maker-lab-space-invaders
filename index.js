d344ea94d67856a512575ba094865a3882402565
juancarlos <juanc.jara@pucp.pe>
2015-12-08 18:11:52 -0500

    graphics added

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ var Game = function(canvasId) {
 
   this.bodies = createInvaders(this).concat(new Player(this));
   this.shootSound = document.getElementById('shoot-sound');
+  var sprite = new Image();
 
   var self = this;
   var tick = function() {
     self.update();
-    self.draw(screen);
+    self.draw(screen, sprite);
     requestAnimationFrame(tick);
   };
 
-  tick();
+  sprite.onload = tick;
+  sprite.src = 'spritesheet.png';
+
 };
 
 Game.prototype = {
@@ -43,29 +46,38 @@ Game.prototype = {
     }
   },
 
-  draw: function(screen) {
+  draw: function(screen, img) {
     screen.clearRect(0, 0, this.size.x, this.size.y);
     for(var i = 0; i < this.bodies.length; i++) {
-      this.bodies[i].draw(screen);
+      this.bodies[i].draw(screen, img);
     }
   }
 };
 
 var Player = function(game) {
   this.game = game;
-  this.size = {x: 15, y: 15};
+  this.image = {x: 0, y: 5, width: 64, height: 64};
+  this.size = {x: 20, y: 20};
   this.center = {x: game.size.x / 2, y: game.size.y - this.size.y};
   this.keyboarder = new Keyboarder();
+  this.shootInterval = 500;
+  this.lastShoot = (new Date()).getTime();
 };
 
 Player.prototype = {
 
   update: function() {
+
     if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
       this.center.x -= 2;
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
       this.center.x += 2;
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+
+      var now = (new Date()).getTime();
+      if (this.lastShoot + this.shootInterval > now) return;
+      this.lastShoot = now;
+
       var bullet = new Bullet({x: this.center.x,
                                y: this.center.y - this.size.y - 10},
                               {x: 0, y: -7});
@@ -76,8 +88,8 @@ Player.prototype = {
 
   },
 
-  draw: function(screen) {
-    drawRect(screen, this);
+  draw: function(screen, sprite) {
+    drawImage(screen, sprite, this);
   }
 
 };
@@ -101,9 +113,10 @@ Bullet.prototype = {
 
 };
 
-var Invader = function(game, center) {
+var Invader = function(game, image, center) {
   this.game = game;
-  this.size = {x: 15, y: 15};
+  this.image = image;
+  this.size = {x: 20, y: 20};
   this.center = center;
   this.patrolX = 0;
   this.speedX = 0.3;
@@ -119,7 +132,7 @@ Invader.prototype = {
     if (Math.random() > 0.995 &&
         !this.game.invadersBellow(this)) {
       var bullet = new Bullet({x: this.center.x,
-                               y: this.center.y + this.size.y},
+                               y: this.center.y + this.size.y/2},
                               {x: Math.random() - 0.5, y: 2});
       this.game.addBody(bullet);
     }
@@ -128,8 +141,9 @@ Invader.prototype = {
     this.patrolX += this.speedX;
   },
 
-  draw: function(screen) {
-    drawRect(screen, this);
+  draw: function(screen, sprite) {
+    drawImage(screen, sprite, this);
+    // drawRect(screen, this);
   }
 
 };
@@ -152,12 +166,16 @@ var Keyboarder = function() {
   this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
 };
 
+
+
 var createInvaders = function(game) {
   var invaders = [];
   for(var i = 0; i < 24; i++) {
     var x = 35 + (i % 8) * 30;
     var y = 35 + (i % 3) * 30;
-    invaders.push(new Invader(game, {x: x, y: y}));
+    var imgX = Math.floor(Math.random() * 11) * 39 + 5;
+    invaders.push(new Invader(game,{x: imgX, y: 84,width: 39, height: 39},
+                              {x: x, y: y}));
   }
   return invaders;
 };
@@ -170,7 +188,15 @@ var colliding = function(b1, b2){
            b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2 );
 };
 
+var drawImage = function(screen, sprite, body) {
+  screen.drawImage(sprite, body.image.x, body.image.y, body.image.width,
+                   body.image.height, body.center.x - body.size.x,
+                   body.center.y - body.size.y, body.size.x,
+                   body.size.y);
+};
+
 var drawRect = function(screen, body) {
+  screen.fillStyle = 'white';
   screen.fillRect(body.center.x - body.size.x,
                   body.center.y - body.size.y,
                   body.size.x, body.size.y);
